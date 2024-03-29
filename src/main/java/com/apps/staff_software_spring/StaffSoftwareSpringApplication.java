9fe7066db833c3df020427bde9d30041dbe203a5
adisonzenemij <adisonzenemij@gmail.com>
2024-03-29 09:01:26 -0500

    Feat: Proyecto Desarrollado

---
+++ src/main/java/com/apps/staff_software_spring/StaffSoftwareSpringApplication.java
@@ -0,0 +1,13 @@
+package com.apps.staff_software_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffSoftwareSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffSoftwareSpringApplication.class, args);
+	}
+
+}
