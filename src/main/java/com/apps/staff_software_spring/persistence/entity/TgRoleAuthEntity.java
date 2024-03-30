e7d801a9283314734deb026bbebfc16cf44500b7
adisonzenemij <adisonzenemij@gmail.com>
2024-03-29 19:55:32 -0500

    Feat: Modulo Tg Role Auth

---
+++ src/main/java/com/apps/staff_software_spring/persistence/entity/TgRoleAuthEntity.java
@@ -0,0 +1,40 @@
+package com.apps.staff_software_spring.persistence.entity;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tg_role_auth")
+@EntityListeners({AuditingEntityListener.class})
+@Getter
+@Setter
+@NoArgsConstructor
+public class TgRoleAuthEntity extends AuditoryEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_register", nullable = true)
+    private Integer idRegister;
+
+    @Column(name = "cd_name", nullable = false, length = 255, unique = false)
+    private String cdName;
+
+    @Override
+    public String toString() {
+        return "TgUserIpEntity{" +
+            "idRegister=" + idRegister +
+            ", cdName='" + cdName + '\'' +
+            '}';
+    }
+}
