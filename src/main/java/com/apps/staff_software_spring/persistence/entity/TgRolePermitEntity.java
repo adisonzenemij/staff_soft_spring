085d3a22bbee1a88b16f888118054262632003bd
adisonzenemij <adisonzenemij@gmail.com>
2024-03-29 20:41:23 -0500

    Feat: Modulo Tg Role Permit

---
+++ src/main/java/com/apps/staff_software_spring/persistence/entity/TgRolePermitEntity.java
@@ -0,0 +1,82 @@
+package com.apps.staff_software_spring.persistence.entity;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tg_role_permit")
+@EntityListeners({AuditingEntityListener.class})
+@Getter
+@Setter
+@NoArgsConstructor
+public class TgRolePermitEntity extends AuditoryEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_register", nullable = true)
+    private Integer idRegister;
+
+    //@ManyToOne(fecth = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(
+        name = "tg_role_action",
+        referencedColumnName = "id_register",
+        insertable = false,
+        updatable = false
+    )
+    //@JsonIgnore
+    @OrderBy("id_register ASC")
+    //@OrderBy("id_register DESC")
+    private TgRoleActionEntity tgRoleAction;
+
+    //@ManyToOne(fecth = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(
+        name = "tg_role_auth",
+        referencedColumnName = "id_register",
+        insertable = false,
+        updatable = false
+    )
+    //@JsonIgnore
+    @OrderBy("id_register ASC")
+    //@OrderBy("id_register DESC")
+    private TgRoleAuthEntity tgRoleAuth;
+
+    //@ManyToOne(fecth = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(
+        name = "tg_role_data",
+        referencedColumnName = "id_register",
+        insertable = false,
+        updatable = false
+    )
+    //@JsonIgnore
+    @OrderBy("id_register ASC")
+    //@OrderBy("id_register DESC")
+    private TgRoleDataEntity tgRoleData;
+
+    @Override
+    public String toString() {
+        return "TgRolePermitEntity{" +
+            "idRegister=" + idRegister +
+            ", tgRoleAction='" + tgRoleAction + '\'' +
+            ", tgRoleAuth='" + tgRoleAuth + '\'' +
+            ", tgRoleData='" + tgRoleData + '\'' +
+            '}';
+    }
+}
