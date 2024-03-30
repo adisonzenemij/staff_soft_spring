c1f76401866a0279e01e9ccb5c8654c454540644
adisonzenemij <adisonzenemij@gmail.com>
2024-03-29 19:42:05 -0500

    Feat: Configuracion Aplicacion

--- src/main/java/com/apps/staff_software_spring/StaffSoftwareSpringApplication.java
+++ src/main/java/com/apps/staff_software_spring/StaffSoftwareSpringApplication.java
@@ -2,8 +2,14 @@ package com.apps.staff_software_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories
+@EntityScan("com.apps.staff_software_spring.persistence.entity")
 public class StaffSoftwareSpringApplication {
 
 	public static void main(String[] args) {
