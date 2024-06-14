a2287646bbdbd62b061b758ad3fbf16e708bc1b5
adisonzenemij <adisonzenemij@gmail.com>
2024-06-13 23:36:30 -0500

    Feat: Modulo Sy Generic Option

--- src/main/java/com/apps/staff_software_spring/persistence/entity/SyGenericOptionEntity.java
+++ src/main/java/com/apps/staff_software_spring/persistence/entity/SyGenericOptionEntity.java
@@ -1,5 +1,41 @@
 package com.apps.staff_software_spring.persistence.entity;
 
-public class SyGenericOptionEntity {
+import java.io.Serializable;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "sy_generic_option")
+@EntityListeners({ AuditingEntityListener.class })
+@Getter
+@Setter
+@NoArgsConstructor
+public class SyGenericOptionEntity extends AuditoryEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_register", nullable = false)
+    private Integer idRegister;
+
+    @Column(name = "cd_value", nullable = false, length = 2, unique = false)
+    private String cdValue;
+
+    // Imprimir Atributos
+    @Override
+    public String toString() {
+        return "SyAuthenticationEntity{" +
+            "idRegister=" + idRegister +
+            ", cdValue='" + cdValue + '\'' +
+            '}';
+    }
 }
