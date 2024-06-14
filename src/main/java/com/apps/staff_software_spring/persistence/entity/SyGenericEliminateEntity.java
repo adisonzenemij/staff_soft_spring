99d18792d7af90f607b40c76c37df27013bacb30
adisonzenemij <adisonzenemij@gmail.com>
2024-06-13 23:35:04 -0500

    Feat: Modulo Sy Generic Eliminate

--- src/main/java/com/apps/staff_software_spring/persistence/entity/SyGenericEliminateEntity.java
+++ src/main/java/com/apps/staff_software_spring/persistence/entity/SyGenericEliminateEntity.java
@@ -1,5 +1,41 @@
 package com.apps.staff_software_spring.persistence.entity;
 
-public class SyGenericEliminateEntity {
+import java.io.Serializable;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "sy_generic_eliminate")
+@EntityListeners({ AuditingEntityListener.class })
+@Getter
+@Setter
+@NoArgsConstructor
+public class SyGenericEliminateEntity extends AuditoryEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_register", nullable = false)
+    private Integer idRegister;
+
+    @Column(name = "cd_name", nullable = false, length = 255, unique = false)
+    private String cdName;
+
+    // Imprimir Atributos
+    @Override
+    public String toString() {
+        return "SyAuthenticationEntity{" +
+            "idRegister=" + idRegister +
+            ", cdName='" + cdName + '\'' +
+            '}';
+    }
 }
