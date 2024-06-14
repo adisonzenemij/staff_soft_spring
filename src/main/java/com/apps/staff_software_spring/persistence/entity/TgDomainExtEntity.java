9c0fa33bbc0e70fec7334047e7bb93754be4207c
adisonzenemij <adisonzenemij@gmail.com>
2024-06-13 23:42:22 -0500

    Feat: Modulo Tg Domain Ext

--- src/main/java/com/apps/staff_software_spring/persistence/entity/TgDomainExtEntity.java
+++ src/main/java/com/apps/staff_software_spring/persistence/entity/TgDomainExtEntity.java
@@ -1,5 +1,41 @@
 package com.apps.staff_software_spring.persistence.entity;
 
-public class TgDomainExtEntity {
+import java.io.Serializable;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tg_domain_ext")
+@EntityListeners({ AuditingEntityListener.class })
+@Getter
+@Setter
+@NoArgsConstructor
+public class TgDomainExtEntity extends AuditoryEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_register", nullable = false)
+    private Integer idRegister;
+
+    @Column(name = "cd_value", nullable = false, length = 15, unique = false)
+    private String cdValue;
+
+    // Imprimir Atributos
+    @Override
+    public String toString() {
+        return "TgDomainExtEntity{" +
+            "idRegister=" + idRegister +
+            ", cdValue='" + cdValue + '\'' +
+            '}';
+    }
 }
