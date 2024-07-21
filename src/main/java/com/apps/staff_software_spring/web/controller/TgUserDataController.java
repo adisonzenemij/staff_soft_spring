94c6a6eeb070b3d0f275fee60b79d1c5b68b608c
Adison Jimenez <adisonzenemij@gmail.com>
2024-07-21 04:00:47 +0000

    Feat: Modulo Usuarios Datos

--- src/main/java/com/apps/staff_software_spring/web/controller/TgUserDataController.java
+++ src/main/java/com/apps/staff_software_spring/web/controller/TgUserDataController.java
@@ -95,25 +95,25 @@ public class TgUserDataController {
     // Obtener todos los registros con paginacion
     @GetMapping(value = "/page/all")
     public ResponseEntity<Page<TgUserDataEntity>> pageAll(
-        @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "10") int elements
+        @RequestParam(defaultValue = "0") int sheet,
+        @RequestParam(defaultValue = "10") int row
     ) {
         return ResponseEntity.ok(
-            this.tgUserDataService.pageAll(page, elements)
+            this.tgUserDataService.pageAll(sheet, row)
         );
     }
 
     // Obtener todos los registros con paginacion y ordenacion
     @GetMapping(value = "/page/sort")
     public ResponseEntity<Page<TgUserDataEntity>> pageSortCol(
-        @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "10") int elements,
+        @RequestParam(defaultValue = "0") int sheet,
+        @RequestParam(defaultValue = "10") int row,
         @RequestParam(defaultValue = "idRegister") String sortBy,
         @RequestParam(defaultValue = "ASC") String sortDir
     ) {
         return ResponseEntity.ok(
             this.tgUserDataService.pageSortCol(
-                page, elements, sortBy, sortDir
+                sheet, row, sortBy, sortDir
             )
         );
     }
